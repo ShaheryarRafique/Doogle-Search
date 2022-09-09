e8ab77d34e25e2f40813c7f4a409a21e48bc5063
ShaheryarRafique <shaheryar.rafique100@gmail.com>
2022-09-10 00:41:41 +0500

    Update Case Conventions

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const searchEl = document.getElementById("search");
 
 searchEl.addEventListener("keyup", function(e) {
     const nameCollectionOfDom = document.getElementsByClassName("list");
-    const searchQuery = e.target.value;
+    const searchQuery = e.target.value.toLowerCase();
 
     for(let i = 0; i < nameCollectionOfDom.length; i++) {
-        const userName = nameCollectionOfDom[i].textContent;
+        const userName = nameCollectionOfDom[i].textContent.toLowerCase();
 
         if(userName.includes(searchQuery)) {
             nameCollectionOfDom[i].style.display = "block";
